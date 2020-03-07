7566635137b899589fab33638f9679565398fa5e
Aaron Alejandro Servin Cornejo <servincord1@gmail.com>
2020-03-07 13:57:16 -0600

    Updates

---
+++ precache-manifest.96f49e5be72eba6e8e0ab8e947a2982b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541e7d807ae3dff4763cd2f9f720073f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c34d5b209a1a0566b855",
+    "url": "/monsters-rolodex/static/css/main.4bc451c5.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c34d5b209a1a0566b855",
+    "url": "/monsters-rolodex/static/js/main.e3bbf248.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
